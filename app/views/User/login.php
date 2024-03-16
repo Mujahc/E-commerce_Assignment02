70db429681f8abb3dc2c5799c1794a420b83dbb2
Johann Culla-ag 2147185 <89601928+Mujahc@users.noreply.github.com>
2024-03-16 01:15:15 -0400

    .

--- app/views/User/login.php
+++ app/views/User/login.php
@@ -15,8 +15,8 @@
 			</div>
 
 			<div class="form-group">
-				<input type="submit" name="action" value="Login" /> 
-				<a href='/User/register'>I have no account, bring me to the registration page</a>
+				<input type="submit" name="action" value="Login" />
+				<a href='/Person/register'>I have no account, bring me to the registration page</a>
 			</div>
 		</form>
 	</div>
